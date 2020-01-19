5197a978d87e9a2170602a0380059fe46180841f
ElimuStuart <stuartelimu@gmail.com>
2020-01-19 17:03:03 +0300

    ready for deployment to heroku

--- RolexChap/settings.py
+++ RolexChap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
